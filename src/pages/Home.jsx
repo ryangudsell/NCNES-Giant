efc783d5ad0352deef1d05abf02d79c57123f487
Ryan Gudsell <ryangudsell@gmail.com>
2023-12-01 09:36:10 +1300

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     <div className='nes-container with-title is-dark'>
       <p className='title'>Home</p>
       <p>Welcome to the Home page</p>
+      <button className='nes-btn'>Contact</button>
+      <button className='nes-btn'>Shop</button>
+      {/* Get and map 3 products here, like a featured products section */}
     </div>
   )
 }
