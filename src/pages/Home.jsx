66aab5a52308fe7658229255ed2a5a256e79fd53
Ryan Gudsell <ryangudsell@gmail.com>
2023-12-01 09:55:37 +1300

    Contact Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <div className='nes-container with-title is-dark'>
       <p className='title'>Home</p>
       <p>Welcome to the Home page</p>
-      <button className='nes-btn'>Contact</button>
-      <button className='nes-btn'>Shop</button>
+      <button className='nes-btn'
+      onClick={() => navigate("/contact")}>Contact</button>
+      <button className='nes-btn'
+      onClick={() => navigate("/shop")}>Shop</button>
       {/* Get and map 3 products here, like a featured products section */}
     </div>
   )
