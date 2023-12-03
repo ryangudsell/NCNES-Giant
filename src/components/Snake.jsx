59f0342195f038a7ef63240238bbba9c0f116177
Ryan Gudsell <ryangudsell@gmail.com>
2023-12-04 10:55:39 +1300

    Removed Snake due to unsafe NPM commands

--- src/components/Snake.jsx
+++ src/components/Snake.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import Snake from 'snake-game-react'
 
 const SnakeGame = () => {
   return (
-      <Snake
-        color1="#248ec2"
-        color2="#1d355e"
-        backgroundColor="#ebebeb"
-      />
+    <></>
   )
 }
 
