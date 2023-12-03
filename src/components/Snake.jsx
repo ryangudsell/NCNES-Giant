6109fa94cf4cd25bac5ce6c800c99fd1fddaf647
Ryan Gudsell <ryangudsell@gmail.com>
2023-12-04 12:06:06 +1300

    Snake Game Added

--- src/components/Snake.jsx
+++ src/components/Snake.jsx
@@ -1,9 +1,126 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+const ROWS = 13;
+const COLS = 20;
+
+const Direction = {
+  UP: 'UP',
+  DOWN: 'DOWN',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+};
 
 const SnakeGame = () => {
-  return (
-    <></>
-  )
-}
+  const generateRandomPosition = () => {
+    const row = Math.floor(Math.random() * ROWS);
+    const col = Math.floor(Math.random() * COLS);
+    return { row, col };
+  };
 
-export default SnakeGame
+  const [snake, setSnake] = useState([{ row: 0, col: 0 }]);
+  const [food, setFood] = useState(generateRandomPosition());
+  const [direction, setDirection] = useState(Direction.RIGHT);
+
+  const handleKeyPress = (e) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        setDirection(Direction.UP);
+        break;
+      case 'ArrowDown':
+        setDirection(Direction.DOWN);
+        break;
+      case 'ArrowLeft':
+        setDirection(Direction.LEFT);
+        break;
+      case 'ArrowRight':
+        setDirection(Direction.RIGHT);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const moveSnake = () => {
+    const newSnake = [...snake];
+    const head = { ...newSnake[0] };
+
+    switch (direction) {
+      case Direction.UP:
+        head.row = (head.row - 1 + ROWS) % ROWS;
+        break;
+      case Direction.DOWN:
+        head.row = (head.row + 1) % ROWS;
+        break;
+      case Direction.LEFT:
+        head.col = (head.col - 1 + COLS) % COLS;
+        break;
+      case Direction.RIGHT:
+        head.col = (head.col + 1) % COLS;
+        break;
+      default:
+        break;
+    }
+
+    newSnake.unshift(head);
+
+    if (head.row === food.row && head.col === food.col) {
+      setFood(generateRandomPosition());
+    } else {
+      newSnake.pop();
+    }
+
+    setSnake(newSnake);
+  };
+
+  const checkCollision = () => {
+    const head = snake[0];
+    return snake.slice(1).some((segment) => segment.row === head.row && segment.col === head.col);
+  };
+
+  const resetGame = () => {
+    setSnake([{ row: 0, col: 0 }]);
+    setFood(generateRandomPosition());
+    setDirection(Direction.RIGHT);
+  };
+
+  useEffect(() => {
+    const handle = setInterval(() => {
+      moveSnake();
+      if (checkCollision()) {
+        clearInterval(handle);
+        alert('Game Over! Press OK to play again.');
+        resetGame();
+      }
+    }, 250);
+
+    return () => clearInterval(handle);
+  }, [snake, direction]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {Array.from({ length: ROWS }).map((_, rowIndex) => (
+            <tr key={rowIndex}>
+              {Array.from({ length: COLS }).map((_, colIndex) => (
+                <td
+                  key={colIndex}
+                  className={`cell ${snake.some((segment) => segment.row === rowIndex && segment.col === colIndex) ? 'snake' : ''} ${
+                    food.row === rowIndex && food.col === colIndex ? 'food' : ''
+                  }`}
+                ></td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SnakeGame;
