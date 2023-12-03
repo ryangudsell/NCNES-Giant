b7c1bd9b2f8e0bcb5b67e4c0e1195d1edcf45674
Ryan Gudsell <ryangudsell@gmail.com>
2023-12-04 11:45:19 +1300

    Added Snake Back

--- src/components/Snake.jsx
+++ src/components/Snake.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Snake from 'snake-game-react'
 
 const SnakeGame = () => {
   return (
-    <></>
+      <Snake
+        color1="darkorchid"
+        color2="orangered"
+        backgroundColor="#ebebeb"
+      />
   )
 }
 
