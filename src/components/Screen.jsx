1d7813b98fb7e375601474e6224f815533a86e8f
Jon3sy <ethan.jonesshark@gmail.com>
2023-12-04 10:31:07 +1300

    added snake game

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home"
 import Policy from "../pages/Policy"
 import Shop from "../pages/Shop"
 import Contact from "../pages/Contact"
+import SnakeGame from './Snake'
 
 const Screen = () => {
   return (
@@ -13,6 +14,7 @@ const Screen = () => {
         <Route exact path="/policy" element={<Policy />} />
         <Route exact path="/shop" element={<Shop />} />
         <Route exact path="/contact" element={<Contact />} />
+        <Route exact path='/snake' element={<SnakeGame/>}/>
       </Routes>
     </article>
   )
