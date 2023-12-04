df69fd81b1e338b15511e362449bb429b66d415f
Ryan Gudsell <ryangudsell@gmail.com>
2023-12-05 09:51:14 +1300

    Added JSON data

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 const Shop = () => {
+  
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get("./json/products.json")
+    .then((res) => {
+      console.log(res);
+      setProducts(res.data)
+      setLoading(false)
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className='nes-container with-title darkmode-toggle is-dark'>
       <p className='title'>Shop</p>
